387db7ab044bdb2797e244c5fff447bd0db7751a
Khaliakbarov, Rafkhad <rafkhad.khaliakbarov@stud.tu-darmstadt.de>
2021-11-16 20:17:18 +0100

    Added gray alert box to display tasks. Added method to print tasks at the loading of website.

--- src/app/todo/todo.component.ts
+++ src/app/todo/todo.component.ts
@@ -8,12 +8,20 @@ import { Component, OnInit } from '@angular/core';
 export class TodoComponent implements OnInit {
 
   tasks:string[] = [];
+  tasksToDisplay: string = "";
 
   constructor() { }
 
   ngOnInit(): void {
     this.tasks.push("Wake up at 6 a.m.");
     this.tasks.push("Going to bed at 11 p.m.");
+    this.printTasks();
+  }
+
+  printTasks(): void {
+    var result: string = "";
+    this.tasks.forEach((value: string) => {result += "<li>" + value + "</li>"})
+    this.tasksToDisplay = result;
   }
 
 }
